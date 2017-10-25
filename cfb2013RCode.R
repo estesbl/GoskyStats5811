e8f7204a8539e6a8ba0ece7fe9ebb6ac064525fb
unknown <W7F10S11@MAT-WAL303-37.appd.appstate.edu>
2017-10-25 16:27:46 -0400

    The Final Draft

--- cfb2013RCode.R
+++ cfb2013RCode.R
@@ -1,34 +1,43 @@
 Data <- read.csv('cfb2013lines.csv', header = TRUE)
 Data <- as.data.frame(Data)
 names(Data) <- c('Date', 'Visitor', 'VisitorScore', 'HomeTeam', 
-                'HomeScore', 'BetLine', 'WinMargin', 'PointMargin', 'BetLineDiff', 'VisitorWin')
+                'HomeScore', 'PointSpread', 'WinMargin', 'PointMargin', 'PointSpreadDiff', 'VisitorWin')
 head(Data)
 tail(Data)
 
-hist(Data$BetLineDiff, breaks = 25, xlab = "Bet Difference", ylab = "Frequency", col = "dark green", main = "Histogram of Bet Line Difference")
-summary(Data$BetLineDiff)
-mean(Data$BetLineDiff)
-sd(Data$BetLineDiff)
-error <- ((sd(Data$BetLineDiff)/sqrt(702))*1.96)
-leftCI <- mean(Data$BetLineDiff)-error
-rightCI <- mean(Data$BetLineDiff)+error
+hist(Data$PointSpreadDiff, breaks = 25, xlab = "Point Spread Difference", ylab = "Frequency", col = "dark green", main = "Histogram of Point Spread Difference")
+summary(Data$PointSpreadDiff)
+mean(Data$PointSpreadDiff)
+sd(Data$PointSpreadDiff)
+error <- ((sd(Data$PointSpreadDiff)/sqrt(702))*1.96)
+leftCI <- mean(Data$PointSpreadDiff)-error
+rightCI <- mean(Data$PointSpreadDiff)+error
 leftCI
 rightCI
 
-boxplot(Data$BetLineDiff)
+boxplot(Data$PointSpreadDiff, ylab = "Point Spread Difference", col = "dark green")
 
-hist(Data2$BetLineDiff, breaks = 25, xlab = "Bet Difference", ylab = "Frequency", col = "red", main = "Histogram of Bet Line Difference")
-summary(Data2$BetLineDiff)
-mean(Data2$BetLineDiff)
-sd(Data2$BetLineDiff)
-error2 <- ((sd(Data2$BetLineDiff)/sqrt(694))*1.96)
-leftCI2 <- mean(Data2$BetLineDiff)-error
-rightCI2 <- mean(Data2$BetLineDiff)+error
+Data2 <- read.csv('cfb2013linesw-oOutliers.csv', header = TRUE)
+Data2 <- as.data.frame(Data2)
+names(Data2) <- c('Date', 'Visitor', 'VisitorScore', 'HomeTeam', 
+                  'HomeScore', 'PointSpread', 'WinMargin', 'PointMargin', 'PointSpreadDiff', 'VisitorWin')
+head(Data2)
+
+hist(Data2$PointSpreadDiff, breaks = 25, xlab = "Point Spread Difference", ylab = "Frequency", col = "red", main = "Histogram of Point Spread Difference w/o Outliers")
+summary(Data2$PointSpreadDiff)
+mean(Data2$PointSpreadDiff)
+sd(Data2$PointSpreadDiff)
+
+boxplot(Data2$PointSpreadDiff, ylab = "Point Spread Difference w/o Outliers", col = "red")
+
+error2 <- ((sd(Data2$PointSpreadDiff)/sqrt(694))*1.96)
+leftCI2 <- mean(Data2$PointSpreadDiff)-error
+rightCI2 <- mean(Data2$PointSpreadDiff)+error
 leftCI2
 rightCI2
 
-plot(Data$BetLine, Data$PointMargin, pch = 17, xlab = "Bet Line", ylab = "Point Margin", col = "blue")
-fit <- lm(Data$BetLine ~ Data$PointMargin)
+plot(Data$PointSpread, Data$PointMargin, pch = 17, xlab = "Bet Line", ylab = "Point Margin", col = "blue")
+fit <- lm(Data$PointSpread ~ Data$PointMargin)
 fit
 abline(fit, col = "red")
 summary(fit)
@@ -36,17 +45,3 @@ summary(fit)
 plot(Data$BetLine, Data$WinMargin)
 
 plot(Data$PointMargin, Data$BetLine)
-
-Data2 <- read.csv('cfb2013linesw-oOutliers.csv', header = TRUE)
-Data2 <- as.data.frame(Data2)
-names(Data2) <- c('Date', 'Visitor', 'VisitorScore', 'HomeTeam', 
-                 'HomeScore', 'BetLine', 'WinMargin', 'PointMargin', 'BetLineDiff', 'VisitorWin')
-head(Data2)
-boxplot(Data2$BetLineDiff)
-summary(Data2$BetLineDiff)
-plot(Data2$BetLineDiff, Data2$PointMargin)
-fit2 <- lm(Data2$BetLineDiff ~ Data2$PointMargin)
-fit2
-abline(fit2)
-Data$WinningMargin
-head(Data)
